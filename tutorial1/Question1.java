89134d3a65d9dabb870da3d9413c922018fa2a03
gentasankara <62679608+gentasankara@users.noreply.github.com>
2020-10-18 14:41:00 +0800

    Update Question1.java

--- tutorial1/Question1.java
+++ tutorial1/Question1.java
@@ -12,7 +12,7 @@ package tutorial1;
 import java.util.*;
 public class Question1 {
 
-  public static void main(String[] argv)
+  public static void main(String[] args)
     {
         int survey;
         double paid=0;
